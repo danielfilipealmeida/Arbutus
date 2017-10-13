cbfebde6862f8d7b61c0cf99de12afef9c5b45a4
Daniel Almeida <danielf.almeida@gmail.com>
2017-10-13 19:51:56 +0100

    Splitter

--- src/GUI/Splitter.cpp
+++ src/GUI/Splitter.cpp
@@ -7,3 +7,28 @@
 //
 
 #include "Splitter.hpp"
+
+Splitter::Splitter() {
+    
+}
+
+Splitter::~Splitter()  {
+    
+}
+
+
+void Splitter::set(json config) {
+    
+}
+
+
+
+void Splitter::update() {
+    
+}
+
+
+
+void Splitter::draw(NVGcontext* vg) {
+    
+}
